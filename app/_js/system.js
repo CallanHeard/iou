ff925265e350a5934755a07463b2223df369ae9a
CallanHeard <c.j.heard@ncl.ac.uk>
2015-09-08 21:29:52 +0100

    Version 0.2.1
    - Payments on dashboard now handled
    - Minor site restructuring

--- app/_js/system.js
+++ app/_js/system.js
@@ -3,13 +3,12 @@ var user; //User object to be instantiated
 //Function for handling log-in
 function login(id) {
 	
-	xmlhttp = new XMLHttpRequest(); //Create new AJAX request object
-	
+	xmlhttp = new XMLHttpRequest();							//Create new AJAX request object
 	xmlhttp.open("GET","../handle.php?login=" + id, false);	//Specify AJAX request
 	xmlhttp.send();											//And send
 	
-	parser	= new DOMParser();
-	userXml	= parser.parseFromString(xmlhttp.responseText, "text/xml");
+	parser	= new DOMParser();											//Parser object for parsing XML data
+	userXml	= parser.parseFromString(xmlhttp.responseText, "text/xml");	//Parse response string into XML document
 	
 	//Retrieve user details from XML
 	var id			= 'id=' 		+ userXml.getElementsByTagName('id')[0].childNodes[0].nodeValue;		//Get ID
@@ -18,6 +17,39 @@ function login(id) {
 	var email		= 'email='		+ userXml.getElementsByTagName('email')[0].childNodes[0].nodeValue;		//Get email
 	
 	document.cookie = id + ',' + firstName + ',' + lastName + ',' + email; //Store details in a cookie
+	
+}
+
+//Function for loading generic page content
+function loadPage() {
+	loadUser();			//Load user details from cookie
+	user.loadProfile();	//Display user profile
+}
+
+//Function for loading page content
+function dashboard() {
+	
+	xmlhttp = new XMLHttpRequest();									//Create new AJAX request object
+	xmlhttp.open("GET","../handle.php?payments=" + user.id, false);	//Specify AJAX request
+	xmlhttp.send();													//And send
+		
+	parser		= new DOMParser();											//Parser object for parsing XML data
+	paymentsXml	= parser.parseFromString(xmlhttp.responseText, "text/xml");	//Parse response string into XML document
+
+	var payments = paymentsXml.getElementsByTagName('payment'); //Get payment elements from XML document
+	
+	//Loop through payment elements
+	for (var i=0; i<payments.length; i++) {
+		
+		//Get payment details
+		var id		= payments[i].childNodes[0].childNodes[0].nodeValue; //ID
+		var name	= payments[i].childNodes[1].childNodes[0].nodeValue; //Name
+		var amount	= payments[i].childNodes[2].childNodes[0].nodeValue; //Amount
+
+		//Create new Payment object with details from XML and add to list of payments
+		document.getElementById('payments').innerHTML += new Payment(id, name, amount);
+			
+	}
 	
 }
 
@@ -40,28 +72,47 @@ function loadUser() {
 	//Instantiate User object with details from cookie
 	user = new User(details['id'], details['firstName'], details['lastName'], details['email']);
 	
-	updatePage(); //Update page with user details
-	
-}
-
-//Function for updating page with user details
-function updatePage() {
-
-	//Update profile details
-	document.getElementById('profile').childNodes[1].src		= user.profileImage	//Update profile image
-	document.getElementById('profile').childNodes[3].innerHTML	= user.fullName;	//Update name
-	document.getElementById('profile').childNodes[5].innerHTML	= user.email;		//Update email
-	
 }
 
 //User class
 function User(id, firstName, lastName, email) {
 	
-	this.id				= id;
-	this.firstName		= firstName;
-	this.lastName		= lastName;
-	this.fullName		= firstName + ' ' + lastName;
-	this.email			= email;
-	this.profileImage	= 'http://lorempixel.com/40/40/people/?' + id;
+	//User properties
+	this.id				= id;											//ID
+	this.firstName		= firstName;									//First name
+	this.lastName		= lastName;										//Last name
+	this.fullName		= firstName + ' ' + lastName;					//Full name
+	this.email			= email;										//Email
+	this.profileImage	= 'http://lorempixel.com/40/40/people/?' + id;	//Link to profile image
+	
+	//Method for displaying the user's profile
+	this.loadProfile = function() {
+		
+		//Generate profile mark-up
+		var profile	 = '<img src="' + this.profileImage + '" alt="' + this.fullName + '"\'s Profile Image"" />';	//Add profile image
+		profile		+= '<p>' + this.fullName + '</p>';																//Add name
+		profile		+= '<p>' + this.email + '</p>';																	//Add email
+		
+		document.getElementById('profile').innerHTML = profile; //Add profile mark-up to page
+		
+	}
+	
+}
+
+//Payment class
+function Payment(id, name, amount) {
+	
+	//Payment properties
+	this.id		= id;		//ID
+	this.name	= name;		//Name
+	this.amount	= amount;	//Amount
+	
+	//Override object 'toString()' method to generate mark-up
+	Payment.prototype.toString = function() {
+		
+		//Return mark-up for displaying payment
+		return '<li><a href="#"><p class="name">' + this.name + '</p><p class="amount">' + this.amount + '</p></a></li>';
+		
+	}
 	
 }
